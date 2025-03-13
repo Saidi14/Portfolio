d32a1b778bf5f18b39531009784a69526048c551
Saidi <sparkinshot@gmail.com>
2025-03-13 06:06:17 +0200

    test

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ function About(){
         <div className="about">
             <div className="aboutText">
                 <h1>Software Developer</h1>
-                <p>Hi! My name is Saidi. I am an aspiring full-stack Developer, 
+                <p>Hello! My name is Saidi. I am an aspiring full-stack Developer, 
                     currently in my final year of studies. 
                     I have experience developing desktop applications and websites.
                 </p>

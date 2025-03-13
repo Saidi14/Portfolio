cf51fcbaeec1d42b81a9b5f8a1c4609e2795fa2a
Saidi <sparkinshot@gmail.com>
2025-03-13 05:56:39 +0200

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+function About(){
+    return(
+        <div className="about">
+            <div className="aboutText">
+                <h1>Software Developer</h1>
+                <p>Hi! My name is Saidi. I am an aspiring full-stack Developer, 
+                    currently in my final year of studies. 
+                    I have experience developing desktop applications and websites.
+                </p>
+            </div>
+            <img src="./Portfolio/home-img.png" alt="Home Image"/>
+        </div>
+    )
+}
+export default About
